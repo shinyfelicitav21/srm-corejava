f6162bb916fa25ba67aeac0a85bdc62db6434c42
shinyfelicitav21 <78897934+shinyfelicitav21@users.noreply.github.com>
2021-06-25 01:05:57 +0530

    Add files via upload

--- Day-04/CommandLine.java
+++ Day-04/CommandLine.java
@@ -1,27 +1,28 @@
 public class CommandLine
 {
-                public static void main(String args[])
+      public static void main(String args[])
+           {
+               int invalid=0;    
+                int number,count=0;
+
+                for(int i=0;i<args.length;i++)
                 {
-                                int invalid=0;    
-                                int number,count=0;
-                                for(int i=0;i<args.length;i++)
+                    try
+                     {
+                        number=Integer.parseInt(args[i]);
+                      }
 
-                                {
-                                                try
-                                                {
-                                                                number=Integer.parseInt(args[i]);
-                                                }
-                                                catch(NumberFormatException e)
-                                                {
-                                                                invalid=invalid+1;             
-                                                               
-                                                                continue;             
-                                                }
-                                                count=count+1;
-                                }
+                     catch(NumberFormatException e)
+                     {
+                         invalid=invalid+1;             
+                         continue;
+                       }
 
-                System.out.println("Valid Elements = "+count);
-                System.out.println("Invalid Elements = "+invalid);
+                       count=count+1;
+                   }
+
+               System.out.println("Valid Elements = "+count);
+               System.out.println("Invalid Elements = "+invalid);
 
             }
 }
