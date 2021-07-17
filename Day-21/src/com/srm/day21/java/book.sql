1fedbef48b85783c53660ad0546b04aa2a354227
shinyfelicitav21 <78897934+shinyfelicitav21@users.noreply.github.com>
2021-07-17 11:06:41 +0530

    Add files via upload

---
+++ Day-21/src/com/srm/day21/java/book.sql
@@ -0,0 +1,7 @@
+INSERT INTO book VALUES(21798,"The Wings of Fire","Dr.A.P.J.Abdul Kalam");
+INSERT INTO book VALUES(2994,"Lord of the Rings","J.R.R. Tolkien");
+INSERT INTO book VALUES(3345,"Harry Potter and the Sorcerer's Stone"," J.K. Rowling");
+INSERT INTO book VALUES(2326,"And Then There Were None","Agatha Christie");
+INSERT INTO book VALUES(61580,"Alice's Adventures in Wonderland","Lewis Carroll");
+
+SELECT * FROM book;
